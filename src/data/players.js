2938c195c7213daca6d1b12c754e017f57d700fa
Juan Arias <juan.henao2306@gmail.com>
2025-11-24 12:28:48 -0500

    Inicio del proyecto: estructura base creada

---
+++ src/data/players.js
@@ -0,0 +1,12 @@
+export const players = [
+  { id: 1, name: "LeBron James", team: "Lakers", age: 39, points: 28, assists: 8, rebounds: 7 },
+  { id: 2, name: "Stephen Curry", team: "Warriors", age: 36, points: 30, assists: 6, rebounds: 5 },
+  { id: 3, name: "Kevin Durant", team: "Suns", age: 35, points: 27, assists: 5, rebounds: 7 },
+  { id: 4, name: "Giannis Antetokounmpo", team: "Bucks", age: 30, points: 29, assists: 5, rebounds: 11 },
+  { id: 5, name: "Nikola Jokic", team: "Nuggets", age: 29, points: 26, assists: 9, rebounds: 12 },
+  { id: 6, name: "Luka Doncic", team: "Mavericks", age: 25, points: 32, assists: 9, rebounds: 8 },
+  { id: 7, name: "Jayson Tatum", team: "Celtics", age: 26, points: 27, assists: 4, rebounds: 8 },
+  { id: 8, name: "Jimmy Butler", team: "Heat", age: 35, points: 23, assists: 5, rebounds: 6 },
+  { id: 9, name: "Kyrie Irving", team: "Mavericks", age: 32, points: 25, assists: 6, rebounds: 5 },
+  { id: 10, name: "Joel Embiid", team: "76ers", age: 30, points: 33, assists: 4, rebounds: 10 }
+];
